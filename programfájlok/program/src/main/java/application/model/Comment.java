31246656a5c2f6c8a6afddaa2c1d7b983c1e3844
Juhász Márk <h263268@stud.u-szeged.hu>
2023-11-21 17:28:40 +0100

    Komment osztály

---
+++ programfájlok/program/src/main/java/application/model/Comment.java
@@ -0,0 +1,61 @@
+package application.model;
+
+public class Comment {
+    private int id;
+    private String date;
+    private String content;
+    private String authorName;
+    private Hir hir;
+    public Comment(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Hir getHir() {
+        return hir;
+    }
+
+    public void setHir(Hir hir) {
+        this.hir = hir;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                ", content='" + content + '\'' +
+                ", authorName='" + authorName + '\'' +
+                ", hir=" + hir +
+                '}';
+    }
+}
